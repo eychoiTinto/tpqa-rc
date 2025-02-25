96cac91650e16029690a5cc034e2bd79489c6349
eychoi <eychoi.tinto@gmail.com>
2025-02-25 16:28:55 +0900

    View More 버튼 경로 수정

--- components/ui/ViewMoreButton.tsx
+++ components/ui/ViewMoreButton.tsx
@@ -3,7 +3,7 @@ import { Button } from './button'
 
 const ViewMoreButton = ({ title }: { title: string }) => {
     return (
-        <a href="/about-us#aboutCase" className="block mt-10 md:mt-[60px] text-center">
+        <a href="/about-us/#aboutCase" className="block mt-10 md:mt-[60px] text-center">
             <Button
                 variant="outline"
                 className="rounded-full px-5 py-[10px] md:py-6 text-[14px] md:text-xl border-[#133761] text-[#1C4A90] hover:bg-primary hover:text-white"
